fdcb14685f7d146f5a6e49966d7d2777b66163af
Rohit Rokaya <109261992+rokayarohit@users.noreply.github.com>
2023-03-08 22:41:34 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$(".btn-like").click(function() {
+		$(this).toggleClass("liked");
+	});
+
+	$(".btn-comment").click(function() {
+		alert("Coming soon!");
+	});
+
+	$(".btn-upload").click(function() {
+		alert("Coming soon!");
+	});
+});
